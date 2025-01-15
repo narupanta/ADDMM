f4fab6f6206877873cd1ec3005a8b606888d2945
narupanta <narunatpantapalin@gmail.com>
2025-01-15 14:55:36 +0100

    update surrogate model (CVGAE)

---
+++ surrogate_model/core/datasetclass.py
@@ -0,0 +1,45 @@
+import json
+import torch
+from torch_geometric.data import Data, Dataset
+from torch_geometric.loader import DataLoader
+import os
+from utils import *
+class MaterialDeformationDataset(Dataset):
+    def __init__(self, data_dir):
+        """
+        Generates synthetic dataset for material deformation use case.
+
+        Args:
+            num_graphs (int): Number of graphs in the dataset.
+            num_nodes (int): Number of nodes per graph.
+            num_features (int): Number of features per node.
+            num_material_params (int): Number of material parameters.
+        """
+        super(MaterialDeformationDataset, self).__init__()
+        self.data_dir = data_dir
+        self.file_name_list = [filename for filename in os.listdir(data_dir) if not os.path.isdir(os.path.join(data_dir, filename))]
+        with open(os.path.join(data_dir, "params_desc/G_K_indices.json"), "r") as file:
+            meta = json.load(file)
+        # Print the content
+        self.meta = meta
+    def __len__(self):
+        return len(self.file_name_list)
+
+    def __getitem__(self, idx):
+        # Randomly generate node features
+        file_name = self.file_name_list[idx]
+        data = np.load(os.path.join(self.data_dir, file_name))
+        i_g = int(file_name.rstrip(".npz").split("_")[2])
+        i_k = int(file_name.rstrip(".npz").split("_")[3])
+
+        decomposed_connectivity = triangles_to_edges(torch.tensor(data['node_connectivity']))['two_way_connectivity']
+        nodal_material_params = [self.meta["G_list"][i_g], self.meta["G_list"][i_k]]
+
+        u = data["u"]
+
+        mesh_pos = torch.tensor(data["mesh_pos"])
+        edge_index = torch.cat((decomposed_connectivity[0].reshape(1, -1), decomposed_connectivity[1].reshape(1, -1)), dim=0)
+        material_params = torch.tensor(nodal_material_params).repeat(mesh_pos.shape[0], 1)  # Material parameter
+        gt_displacement = torch.tensor(u, dtype=torch.float)  # Target node values
+
+        return Data(mesh_pos = mesh_pos, edge_index = edge_index, material_params = material_params, gt_displacement = gt_displacement)
---
+++ surrogate_model/core/utils.py
@@ -0,0 +1,56 @@
+import torch
+
+def compute_and_store_normalization(data):
+    """
+    Computes the mean and standard deviation of the input data and stores them.
+    
+    Args:
+        data (torch.Tensor): Input data tensor of shape [num_samples, num_features].
+        save_path (str): File path to save the normalization parameters.
+        
+    Returns:
+        torch.Tensor: Normalized data.
+    """
+    mean = data.mean(dim=0)
+    std = data.std(dim=0)
+    
+    # Return normalized data
+    return (data - mean) / std
+
+def denormalize_data(normalized_data, mean, std):
+    """
+    Denormalizes the input data using previously stored mean and standard deviation.
+    
+    Args:
+        normalized_data (torch.Tensor): Normalized data tensor of shape [num_samples, num_features].
+        save_path (str): File path to load the normalization parameters.
+        
+    Returns:
+        torch.Tensor: Denormalized data.
+    """
+    
+    # Denormalize data
+    return normalized_data * std + mean
+
+
+def triangles_to_edges(faces):
+    """Computes mesh edges from triangles."""
+    # collect edges from triangles
+    edges = torch.cat((faces[:, 0:2],
+                           faces[:, 1:3],
+                           torch.stack((faces[:, 2], faces[:, 0]), dim=1)), dim=0)
+    # those edges are sometimes duplicated (within the mesh) and sometimes
+    # single (at the mesh boundary).
+    # sort & pack edges as single tf.int64
+    receivers, _ = torch.min(edges, dim=1)
+    senders, _ = torch.max(edges, dim=1)
+
+    packed_edges = torch.stack((senders, receivers), dim=1)
+    unique_edges = torch.unique(packed_edges, return_inverse=False, return_counts=False, dim=0)
+    senders, receivers = torch.unbind(unique_edges, dim=1)
+    senders = senders.to(torch.int64)
+    receivers = receivers.to(torch.int64)
+
+    two_way_connectivity = (torch.cat((senders, receivers), dim=0), torch.cat((receivers, senders), dim=0))
+    return {'two_way_connectivity': two_way_connectivity, 'senders': senders, 'receivers': receivers}
+    
